59522eccada627a0daed3894b83bee24934f7387
Ignacio Mazzara <ignaciomazzara@hotmail.com>
2014-06-26 22:23:17 -0300

    equeleto parte principal parte 2

--- TP-Parte2/TP-Parte2/Principal.cbl
+++ TP-Parte2/TP-Parte2/Principal.cbl
@@ -85,44 +85,68 @@
        sd temporal
            data record is reg-temporal.
        01  reg-temporal.
-           03  fecha.
-               05  fecha-dd    pic     99.
-               05  fecha-mm    pic     99.
-               05  fecha-aaaa  pic     9999.
-           03  cho-nro-legajo  pic     x(7).
-           03  cho-turno       pic     x(6). *> "manana", "tarde" etc
-           03  cli-numero      pic     x(8).
-           03  cli-tipo-doc    pic     x.
-           03  cli-nro-doc     pic     x(20).
-           03  cli-direccion   pic     x(30).
+           03  temp-fecha.
+               05  temp-fecha-dd    pic     99.
+               05  temp-fecha-mm    pic     99.
+               05  temp-fecha-aaaa  pic     9999.
+           03  temp-cho-nro-legajo  pic     x(7).
+           03  temp-cho-turno       pic     x(6). *> "manana", "tarde" etc
+           03  temp-cli-numero      pic     x(8).
+           03  temp-cli-tipo-doc    pic     x.
+           03  temp-cli-nro-doc     pic     x(20).
+           03  temp-cli-direccion   pic     x(30).
            
            
        working-storage section.
+       
+       01 chof-estado pic xx.
        
        01 fs-alquileresmae     pic xx.
            88 ok-alq                    value "00".
            88 no-alq                    value "23".
            88 eof-alq                   value "10".
-       
+           
        01 fs-choferes          pic xx.
            88 ok-chof                   value "00".
            88 no-chof                   value "23".
            88 eof-chof                  value "10".
            
+       01 fecha-actual.
+               05  fecha-actual-dd    pic     99.
+               05  fecha-actual-mm    pic     99.
+               05  fecha-actual-aaaa  pic     9999.
+               
        77 fs-rechazos          pic xx.
-       
        77 fs-listado           pic xx.
-       
+       77 fs-temporal          pic xx.
+       77 chof-estado-activo pic xx value 'si'.
+       77 chof-estado-inactivo pic xx value 'no'.
+       77 chofer-actual pic     x(7).
 
        PROCEDURE DIVISION.
-           
-           perform abrir-archivos.
-           *> en algun lado
-           call "BuscarDatosCliente" using alq-nro-doc, .
-           perform cerrar-archivos.
-           
+           perform abrir-clientes.
+           perform abrir-choferes.
+           perform abrir-temporal.
+           perform sort-section.
+           perform cerrar-choferes.
+           perform cerrar-clientes.
            stop run.
-      
+           
+       abrir-clientes. 
+           call "BuscarDatosCliente" using alq-nro-doc, .
+
+       abrir-choferes.
+       abrir-temporal.
+       cerrar-choferes.
+       cerrar-clientes.
+           call "BuscarDatosCliente" using alq-nro-doc, .
+
+       
+       sort-section.
+           sort temporal ascending key temp-fecha
+               input procedure entrada
+               output procedure salida.
+           
        abrir-archivos.
            open input alquileresmae
                        choferes.
@@ -131,3 +155,61 @@
        cerrar-archivos.
            close alquileresmae
                    choferes.
+                   
+                   
+       *> ENTRADA SECTION
+       entrada.
+           perform abrir-alquileres.
+           perform abrir-rechazados.
+           perform leer-alquileres.
+           perform procesar-alquileres until  eof-alq. 
+           perform cerrar-alquileres. 
+           perform cerrar-rechazados.
+       
+       abrir-alquileres.
+       abrir-rechazados.
+       leer-alquileres.
+       procesar-alquileres.
+           perform leer-choferes.
+           perform inicializar-proceso-choferes.
+           perform procesar-choferes until eof-chof or cho-fecha-desde 
+           > alq-clave or chof-estado is equal to chof-estado-activo.   
+           perform rechazar-alquiler.
+           
+       leer-choferes.
+       inicializar-proceso-choferes.
+       procesar-choferes.
+           perform actualizar-alquileres.
+           call "BuscarDatosCliente" using alq-nro-doc, .
+           perform escribir-arch-temporal.
+           
+       actualizar-alquileres.    
+       escribir-arch-temporal.
+       rechazar-alquiler.
+       cerrar-alquileres.
+       cerrar-rechazados.
+       
+       *> SALIDA SECTION
+       salida.
+           perform abrir-listado.
+           perform leer-temporal.
+           perform procesar-fecha until  fs-temporal is equal to 10. 
+           perform cerrar-listado.  
+           perform cerrar-temporal.
+           
+       abrir-listado.
+       leer-temporal.
+       procesar-fecha.
+           perform procesar-chofer until  fs-temporal is equal to 10 or 
+               fecha-actual is not equal to temp-fecha.
+               
+       procesar-chofer.
+           perform procesar-cliente until  fs-temporal is equal to 10 
+           or fecha-actual is not equal to temp-fecha or chofer-actual 
+           is                                                           
+           not equal to temp-cho-nro-legajo.                            
+         
+       procesar-cliente.
+           
+       cerrar-listado.
+       cerrar-temporal.
